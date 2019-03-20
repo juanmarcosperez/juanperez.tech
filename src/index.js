0552fd7f985a7999873466d3ada8c8c6df8798fe
Juan Perez <juanmarcperez@gmail.com>
2019-03-20 02:17:59 -0500

    new readme

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-//import SideBar from "./components/SideBar";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 //ReactDOM.render(<SideBar />, document.getElementById("sidebar"));
